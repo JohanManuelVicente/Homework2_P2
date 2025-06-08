a810d7c2698d45ef4793dfeb0ad928b34d5a3730
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-06-08 18:06:37 -0400

    Update in Controller

--- HotelParadise/HotelParadise/Controllers/EmployeesController.cs
+++ HotelParadise/HotelParadise/Controllers/EmployeesController.cs
@@ -7,6 +7,17 @@ namespace HotelParadise.Controllers
     //[Route("api/Employees")] Too can be this way.
     public class EmployeesController: ControllerBase
     {
+
+
+        List<HotelParadise.Entities.Employee> Employees;
+        public EmployeesController()
+        {
+            Employees = new List<HotelParadise.Entities.Employee>();
+            Employees.Add(new HotelParadise.Entities.Employee { Id=1, Name = "Empleado1", position="Administrador", Phone="809-821-5154", Date_Admission=DateTime.Today});
+            Employees.Add(new HotelParadise.Entities.Employee { Id = 2, Name = "Empleado2", position = "Cajero", Phone = "829-821-5154", Date_Admission = DateTime.Today });
+            Employees.Add(new HotelParadise.Entities.Employee { Id = 3, Name = "Empleado3", position = "Tecnico", Phone = "849-821-5154", Date_Admission = DateTime.Today });
+        }
+
         [HttpGet]
         public IActionResult GetEmployees()
         {
